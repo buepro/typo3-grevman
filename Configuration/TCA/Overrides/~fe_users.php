21b104acd8d0f3d3dce52e792bad09fe5e8aec64
roman <rb@buechler.pro>
2021-06-22 10:51:40 +0200

    [TASK] Modify TCA for member

---
+++ Configuration/TCA/Overrides/~fe_users.php
@@ -0,0 +1,16 @@
+<?php
+defined('TYPO3') || die('Access denied.');
+
+(static function() {
+    $columns = [
+        'member_groups' => [
+            'config' => [
+                'MM' => 'tx_grevman_group_member_mm',
+                'MM_opposite_field' => 'members',
+            ],
+        ],
+    ];
+
+    $GLOBALS['TCA']['fe_users']['columns'] =
+        array_replace_recursive($GLOBALS['TCA']['fe_users']['columns'], $columns);
+})();
--- ext_tables.sql
+++ ext_tables.sql
@@ -70,14 +70,3 @@ CREATE TABLE tx_grevman_group_member_mm (
 	KEY uid_local (uid_local),
 	KEY uid_foreign (uid_foreign)
 );
-
-CREATE TABLE tx_grevman_member_group_mm (
-	uid_local int(11) unsigned DEFAULT '0' NOT NULL,
-	uid_foreign int(11) unsigned DEFAULT '0' NOT NULL,
-	sorting int(11) unsigned DEFAULT '0' NOT NULL,
-	sorting_foreign int(11) unsigned DEFAULT '0' NOT NULL,
-
-	PRIMARY KEY (uid_local,uid_foreign),
-	KEY uid_local (uid_local),
-	KEY uid_foreign (uid_foreign)
-);
