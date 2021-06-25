cc6e0e9949290e0175c8d1fc4dfcc4677427865c
roman <rb@buechler.pro>
2021-06-25 08:06:22 +0200

    [TASK] Adjust TCA for note

--- Configuration/TCA/Overrides/tx_grevman_domain_model_note.php
+++ Configuration/TCA/Overrides/tx_grevman_domain_model_note.php
@@ -2,19 +2,44 @@
 defined('TYPO3') || die('Access denied.');
 
 (static function() {
-    $columns = [
-        'member' => [
-            'exclude' => true,
-            'label' => 'LLL:EXT:grevman/Resources/Private/Language/locallang_db.xlf:tx_grevman_domain_model_note.member',
-            'config' => [
-                'type' => 'select',
-                'renderType' => 'selectSingle',
-                'foreign_table' => 'fe_users',
-                'readOnly' => true,
+    $config = [
+        'ctrl' => [
+            'label' => 'member',
+            'label_alt' => 'event, text',
+            'label_alt_force' => true,
+        ],
+        'types' => [
+            '1' => ['showitem' => 'sys_language_uid, l10n_parent, l10n_diffsource, hidden, text, event, member'],
+        ],
+        'columns' => [
+            'text' => [
+                'config' => [
+                    'readOnly' => true,
+                ],
+            ],
+            'event' => [
+                'exclude' => true,
+                'label' => 'LLL:EXT:grevman/Resources/Private/Language/locallang_db.xlf:tx_grevman_domain_model_event',
+                'config' => [
+                    'type' => 'select',
+                    'renderType' => 'selectSingle',
+                    'foreign_table' => 'tx_grevman_domain_model_event',
+                    'readOnly' => true,
+                ],
+            ],
+            'member' => [
+                'exclude' => true,
+                'label' => 'LLL:EXT:grevman/Resources/Private/Language/locallang_db.xlf:tx_grevman_domain_model_note.member',
+                'config' => [
+                    'type' => 'select',
+                    'renderType' => 'selectSingle',
+                    'foreign_table' => 'fe_users',
+                    'readOnly' => true,
+                ],
             ],
         ],
     ];
 
-    $GLOBALS['TCA']['tx_grevman_domain_model_note']['columns'] =
-        array_replace_recursive($GLOBALS['TCA']['tx_grevman_domain_model_note']['columns'], $columns);
+    $GLOBALS['TCA']['tx_grevman_domain_model_note'] =
+        array_replace_recursive($GLOBALS['TCA']['tx_grevman_domain_model_note'], $config);
 })();
