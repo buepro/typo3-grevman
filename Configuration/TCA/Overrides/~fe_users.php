b80655f802a39558bcf8041d7acfd389cdf1be28
roman <rb@buechler.pro>
2021-07-12 10:17:50 +0200

    [TASK] Adjust TCA for fe-users

--- Configuration/TCA/Overrides/~fe_users.php
+++ Configuration/TCA/Overrides/~fe_users.php
@@ -10,32 +10,38 @@
 defined('TYPO3') || die('Access denied.');
 
 (static function () {
-    $columns = [
-        'member_groups' => [
-            'config' => [
-                'MM' => 'tx_grevman_group_member_mm',
-                'MM_opposite_field' => 'members',
-            ],
+    $config = [
+        'ctrl' => [
+            'label_alt' => 'first_name, last_name',
+            'label_alt_force' => true,
         ],
-        'registrations' => [
-            'config' => [
-                'appearance' => [
-                    'collapseAll' => 1,
-                    'expandSingle' => 1,
+        'columns' => [
+            'member_groups' => [
+                'config' => [
+                    'MM' => 'tx_grevman_group_member_mm',
+                    'MM_opposite_field' => 'members',
                 ],
             ],
+            'registrations' => [
+                'config' => [
+                    'appearance' => [
+                        'collapseAll' => 1,
+                        'expandSingle' => 1,
+                    ],
+                ],
 
-        ],
-        'notes' => [
-            'config' => [
-                'appearance' => [
-                    'collapseAll' => 1,
-                    'expandSingle' => 1,
+            ],
+            'notes' => [
+                'config' => [
+                    'appearance' => [
+                        'collapseAll' => 1,
+                        'expandSingle' => 1,
+                    ],
                 ],
             ],
         ],
     ];
 
-    $GLOBALS['TCA']['fe_users']['columns'] =
-        array_replace_recursive($GLOBALS['TCA']['fe_users']['columns'], $columns);
+    $GLOBALS['TCA']['fe_users'] =
+        array_replace_recursive($GLOBALS['TCA']['fe_users'], $config);
 })();
