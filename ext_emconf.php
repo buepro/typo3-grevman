b113e18f1e4416cc91cea39a3dd939f9ed66fdfb
roman <rb@buechler.pro>
2021-07-20 12:15:52 +0200

    [TASK] Rise to dev version

--- ext_emconf.php
+++ ext_emconf.php
@@ -9,13 +9,13 @@
 
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Group event manager',
-    'description' => 'Typically used to manage group events in sport clubs. Members can easily register and unregister for events and trainers might notify participants by email. Provides a matrix- list and detail view from events.',
+    'description' => 'Typically used to manage group events in sport clubs. Members can easily register and unregister for events and trainers might notify participants by email. Provides a matrix-, list- and detail view from events.',
     'category' => 'plugin',
     'author' => 'Roman Büchler',
     'author_email' => 'rb@buechler.pro',
     'state' => 'beta',
     'clearCacheOnLoad' => 0,
-    'version' => '0.1.0',
+    'version' => '0.1.1-dev',
     'constraints' => [
         'depends' => [
             'typo3' => '10.4.0-10.4.99',
